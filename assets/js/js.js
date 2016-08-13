b91b4fca4a88627abd0e87bc456978f03beea253
knewhouse <newhousemusic@gmail.com>
2016-08-13 14:59:11 -0700

    added a few things, played with slider

--- assets/js/js.js
+++ assets/js/js.js
@@ -105,6 +105,22 @@ $(document).ready(function(){
   	};
 
 	 // Carousels
+  $("#change-slider").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
     if($("#partners-slider").length>0){
     	$("#partners-slider").owlCarousel({
     		items: 4,
@@ -138,7 +154,6 @@ $(document).ready(function(){
     	});
     };
 
-
   // Causes Filter 
   var $causeslist = $('.causes-list');
    
@@ -163,6 +178,8 @@ $(document).ready(function(){
       filter: selector,
     });
   });
+
+
 
 	
 
